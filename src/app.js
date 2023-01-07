9a10720a643e3d8574e089e12dab4e5d4343abe0
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-01-06 19:49:10 -0600

    melhorias no POST(rota /sign-up). salvando os usuários cadastrados em uma variável global e marcando todos os campos como obrigatórios.

--- src/app.js
+++ src/app.js
@@ -1,18 +1,29 @@
 import express from "express";
 import cors from "cors";
 
-//para persistir os dados, devo utilizar VARIÁVEIS GLOBAIS em memória e conferir os formatos para usuário e tweet
-
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+//variáveis globais
+const register = [];
+const messages = [];
+
 
 //POST sign-up:
 app.post('/sign-up', (req, res) => {
     const data = req.body;
 
-    res.status(200).send(data);
+    if(!data.username || !data.avatar){
+        return res.status(422).send('Todos os campos são obrigatórios!')
+    }
+
+    register.push(data)
+    res.status(200).send('OK');
 });
+
+//POST tweets:
+app.post('/tweets')
 
 
 //ATENÇAO: configurar o servidor para rodar na porta 5000 antes de enviar o projeto!
